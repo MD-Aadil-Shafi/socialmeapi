3bdf4e100feefd18fd9cd0df77f9846f23090030
Aadil <md.aadil.shafi@gmail.com>
2023-03-05 22:47:49 +0530

    new mongoose function

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose')
 
-const dbConnect = mongoose.connect(
-    process.env.MONGO_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    },(error) =>{
-        if(!error){
-            console.log("connected to the MongoDB");
-        }else {
-            console.log(error)
-            console.log("Failed to connect to MongoDB")
-        }
-    }
-);
+const dbConnect = () =>{
+    mongoose.connect(process.env.MONGO_URL)
+    .then((data) =>{
+        console.log(`MongoDb connected: ${data.connection.host}`)
+    }).catch((err)=>{
+        console.log('Unable to connect with mongoDB',err )
+    })
+}
 
 module.exports = dbConnect;
 
